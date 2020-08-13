3776cfadd858aefa2448d1163dc8fba12979dc8c
ZiNotGuess <zinotguess@gmail.com>
2020-08-13 18:44:18 +0300

    Рефакторинг кода

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import datetime
 import time
 import requests
-from SettingsReader import GetParams
+from SettingsReader import Params
 
 
-def VkMethod(methodName, methodParams=None):
+def VkMethod(methodName: str, methodParams: dict = None) -> dict or list or str or int:
     """
-    :param methodName: имя метода
+    :param methodName: название метода
     :param methodParams: параметры метода
-    :return: ответ сервера
+    :return: ответ ВКонтакте
     """
     if methodParams is None:
         methodParams = {}
-    methodParams['v'] = param.apiversion
-    methodParams['access_token'] = param.access_token
+    methodParams['v'] = params.getScriptParams("apiVersion")
+    methodParams['access_token'] = params.getScriptParams("access_token")
     r = requests.post('https://api.vk.com/method/' + methodName, methodParams)
     r_json = r.json()
     if 'error' in r_json:
@@ -22,50 +22,53 @@ def VkMethod(methodName, methodParams=None):
     return r_json['response']
 
 
-param = GetParams().par
+numbers = {'0': '0⃣', '1': '1⃣', '2': '2⃣', '3': '3⃣', '4': '4⃣', '5': '5⃣', '6': '6⃣', '7': '7⃣',
+           '8': '8⃣', '9': '9⃣'}
+params = Params()
 while True:
     try:
-        if param.status:
+        if params.getScriptParams("status"):
             status = ''
-            if param.time:
+            if params.getStatusParams("time"):
                 t = datetime.datetime.now()
                 status += f"🕰 {t.strftime('%H:%M')} | 🗓 {t.strftime('%d.%m.%Y')} | "
 
-            if param.photoprofile and param.photolikecount:
+            if params.getStatusParams("photoProfile") and params.getStatusParams("photoLikeCount"):
                 LikeCount = VkMethod('photos.get', {'album_id': 'profile', 'rev': 1, 'extended': 1, 'count': 1})
                 status += f"❤ На аве: {LikeCount['items'][0]['likes']['count']} | "
 
-            if param.followerscount:
+            if params.getStatusParams("followersCount"):
                 FollowersCount = VkMethod('users.getFollowers', {'count': '1000'})
                 status += f"👥 Подписиков: {FollowersCount['count']} | "
 
-            if param.unreadmmssagecount:
+            if params.getStatusParams("unreadMessagesCount"):
                 MessageCount = VkMethod('account.getCounters', {'filter': 'messages'})
                 status += f"📬 Сообщений: {MessageCount['messages']} | "
 
-            if param.blacklistmembercount:
+            if params.getStatusParams("blackListMemberCount"):
                 memberCount = VkMethod('account.getBanned', {'count': '200'})
                 status += f"⛔ В ЧС: {memberCount['count']} | "
 
-            if param.giftscount:
+            if params.getStatusParams("giftsCount"):
                 gifts = VkMethod('gifts.get', {'count': '200'})
                 status += f"🎁 Подарки: {gifts['count']} | "
 
-            if param.decor:
-                for number in param.decorNumber:
-                    status = status.replace(number, param.decorNumber[number])
+            if params.getStatusParams("decor"):
+                for number in numbers:
+                    status = status.replace(number, numbers[number])
 
             VkMethod("status.set", {"text": status[:-3]})
 
-        if param.eternalonline:
+        if params.getScriptParams("eternalOnline"):
             VkMethod("account.setOnline")
 
-        if param.deleteallfriendsrequests:
+        if params.getScriptParams("deleteAllFriendsRequests"):
             VkMethod("friends.deleteAllRequests")
 
     except Exception as Error:
         t = datetime.datetime.now()  # получение полного времени для ошибки
         print('При установки статуса произошла ошибка. Убедитесь, что все настройки в settings.ini введены правильно'
               f'\nВремя:\n {t.strftime("%H:%M")}\nОшибка:\n{str(Error)}\n{"--" * 5}')
+
     finally:
-        time.sleep(int(param.timetosleep))
+        time.sleep(int(params.getScriptParams("timeToSleep")))
